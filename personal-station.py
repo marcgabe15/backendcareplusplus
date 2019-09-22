c0c6cebca9ab9d6ef5c1d1e1d053bca9971a9a67
Jared Holley <jaredrholley@ufl.edu>
2019-09-22 02:43:33 -0400

    Update personal-station.py

--- personal-station.py
+++ personal-station.py
@@ -4,6 +4,7 @@ import asyncio
 import websockets
 import json
 import pprint
+from meteocalc import Temp, dew_point, heat_index
 from matrix_lite import led
 from matrix_lite import sensors
 
@@ -14,7 +15,7 @@ def detectFall():
     print(data.accel_z)
     print ("--------------------")
 
-    if (abs(data.accel_x) > 1.90 and abs(data.accel_x) < 10):
+    if (abs(data.accel_x) > 1.70 and abs(data.accel_x) < 10):
         print("Possible Fall")
         time.sleep(3)
         data = sensors.imu.read()
@@ -33,14 +34,12 @@ def fallDetected():
         time.sleep(.5)
 
 def sendWebAlert():
+    humidity = sensors.humidity.read().humidity
+    temperature = sensors.pressure.read().temperature - 8
+    hi = heat_index(temperature=temperature, humidity= humidity)
     uri = "https://shellhacks2019-1f061.appspot.com/addMarker"
-    #async with websockets.connect(uri) as websocket:
-    #    await websocket.send(json.dumps([{'temp' : '37.44', 'humidity' : '50.00', 'fall' : 'true', 'hasFlooded' : 'false', 'bodyTest' : 'Can you see clearly now'}]))
-    x = requests.post(uri, json = {'humid' : 37.44, 'heatindex' : 50.00, 'bodytext' : 'SOS', 'fallenAlert' : 'false', 'temp' : 100})
+    x = requests.post(uri, json = {'humid' : int(humidity), 'heatindex' : int(hi), 'bodytext' : 'SOS', 'fallenAlert' : 'true', 'temp' : int(temperature)})
     print(x)
-    #uri = "https://shellhacks2019-1f061.appspot.com/markers"
-    #x = requests.get(uri, data = '')
-    #print(x)
     
 
 def phoneContact():
